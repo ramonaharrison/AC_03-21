05a57c582bd721cb1706ca2bfdcee736b0cd71ba
ramonaharrison <ramona.e.harrison@gmail.com>
2015-03-22 23:56:20 -0400

    classwork 3/21

--- src/nyc/c4q/ramonaharrison/Main.java
+++ src/nyc/c4q/ramonaharrison/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(returnLastWord("Access Code"));
+    }
+
+    public static String returnLastWord(String input) {
+        String[] splitInput = input.split(" ");
+        return splitInput[splitInput.length - 1];
     }
 }
